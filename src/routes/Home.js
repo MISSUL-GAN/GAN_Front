046651ebb25be6d2cb106fbb49ad1f151d8d3b89
nijuy <youjin6325@naver.com>
2022-08-02 23:38:08 +0900

    정렬 필터 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Navigation from "../components/Navigation";
 import './Home.css';
 import Drawing from './Drawing';
@@ -10,6 +10,9 @@ function Home() {
     let code = new URL(window.location.href).searchParams.get("code");
     //const code = useSelector( (state) => state );
     const dispatch = useDispatch();
+    const [filter, setFilter] = useState("random");
+    
+    const pictures = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]; 
 
     useEffect(() => {
         dispatch({ type: '로그인'})
@@ -23,11 +26,24 @@ function Home() {
         }
     }, []);
 
-    const pictures = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]; 
+    const changeFilter = (e) => {
+        setFilter(e.target.value);
+    }
+
+    useEffect(() => {
+        //filter 변수 값이 변경될 때마다 호출되는 함수, 여기서 이미지 서버에 요청 보내면 됨
+        filter == "random" ? console.log("랜덤정렬") : console.log("좋아요순");
+    }, [changeFilter]);
 
     return(
         <>
             <Navigation/>
+
+            <select name="filter" onChange={changeFilter}>
+                <option value="random"> 랜덤 보기 </option>
+                <option value="mostlike"> 좋아요 높은순 </option>
+            </select>
+
             <div className="homecontainer">
                 <div className="drawingBox">
                     {
