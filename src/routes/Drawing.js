e41a22e08ecd0cfc90777a1ca6dc25fd8f7eb3ec
nijuy <youjin6325@naver.com>
2022-08-05 19:26:22 +0900

    둘러보기 옵션 추가 + 상세보기 모달 수정

--- src/routes/Drawing.js
+++ src/routes/Drawing.js
@@ -59,44 +59,50 @@ function Drawing({imgsrc, name}){
 
     return(
         <>
-            <div>
+            <div className="drawing">
                 <img src={imgsrc} alt="" className="img-thumbnail" onClick = {clickImg}/>
                 <div className="titleBox">
                     <p> 그림 {name} </p> 
-                    <button className="like" onClick={ clickLike }> { like ? "♥" : "♡"} </button>
-                    <button className="bookmark" onClick={ clickBookmark }> { bookmark ? "스크랩 취소" : "스크랩" } </button>
+                    <div>
+                        <button className="like" onClick={ clickLike }> { like ? "♥" : "♡"} </button>
+                        <button className="bookmark" onClick={ clickBookmark }> { bookmark ? "스크랩 취소" : "스크랩" } </button>
+                    </div>
                 </div>
              </div>
 
             <div id="zoom-modal" className="drawing-modal">
                 <div className="drawing-modal-window">
-                    <img className="large-drawing" src={imgsrc} alt=""/>
-                    <div className="details">
+                    <div className="drawing-modal-left"> <img className="large-drawing" src={imgsrc} alt=""/> </div>
+                
+                    <div>
                         <p className="drawing-modal-close" onClick={clickClose}> x </p>
-                        <div className="userInfo">
-                            <img src={imgsrc} alt="" className="profileImg" width={50} height={50}/>
-                            <p className="author"> 사용자 닉네임 </p>
-                        </div>
+                        <div className="drawing-modal-right">
+                            
+                            <div className="userInfo">
+                                <img src={imgsrc} alt="" className="profileImg" width={50} height={50}/>
+                                <p className="author"> 사용자 닉네임 </p>
+                            </div>
+                            
+                            <p className="drawing-title"> 제목길이제한8자 </p>
+
+                            <p className="description"> 그림 {name} 설명입니다 이 작품은 사람의 간을 본따서 만든 로고인데요 아주 귀엽게 생겼습니다 으아아아아아아아아아아아아ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ </p>
                         
-                        <p className="drawing-title"> 제목길이제한8자 </p>
-                        <p className="description"> 그림 {name} 설명입니다 이 작품은 사람의 간을 본따서 만든 로고인데요 아주 귀엽게 생겼습니다 으아아아아아아아아아아아아ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ </p>
-                    
-                    
-                        <div className="buttonBox">
-                            <button> 다운로드 </button>
-                            <button> 카톡 공유 </button>
-                            <button onClick={ clickNFT }> OpenSea 통계 정보 </button>
-                            <button className="like" onClick={ clickLike }> { like ? "♥" : "♡"} </button>
-                            <button className="bookmark" onClick={ clickBookmark }> { bookmark ? "스크랩 취소" : "스크랩" } </button>
-                        </div>
+                            <p className="drawing-tag"> #태그1 #태그2 #태그3 </p>
+                            
+                            <div className="buttonBox">
+                                <button> 다운로드 </button>
+                                <button> 카톡 공유 </button>
+                                <button onClick={ clickNFT }> OpenSea 통계 정보 </button>
+                                <button className="like" onClick={ clickLike }> { like ? "♥" : "♡"} </button>
+                                <button className="bookmark" onClick={ clickBookmark }> { bookmark ? "스크랩 취소" : "스크랩" } </button>
+                            </div>
 
-                        <div className="NFTBox">
-                            <br/>
-                            통계정보... 이 작품은 https://opensea.com/fgijsdifg 에 등록된거고..<br/>
-                            가격은 몇 이더리움이고.. 근데 난 코인 안 해서 잘 모르겟음 ㅋㅋ<br/>
-                            음 네 뭐 그렇습니다.......... 어쩌고저쩌고
+                            <div className="NFTBox">
+                                통계정보... 이 작품은 https://opensea.com/fgijsdifg 에 등록된거고..<br/>
+                                가격은 몇 이더리움이고.. 근데 난 코인 안 해서 잘 모르겟음 ㅋㅋ<br/>
+                                음 네 뭐 그렇습니다.......... 어쩌고저쩌고
+                            </div>
                         </div>
-
                     </div>
                 </div>
             </div>        
@@ -106,9 +112,7 @@ function Drawing({imgsrc, name}){
                     <p className="warning-modal-close" onClick={clickAlertClose}> x </p>
                     <div className="alert-content">
                         <p> 로그인이 필요한 서비스입니다. </p>
-                        <a href={AUTH_URL}>
-                            <img src="//k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg" width={"222"} alt="카카오 로그인" />
-                        </a>
+                        <a href={AUTH_URL}> <img src="//k.kakaocdn.net/14/dn/btroDszwNrM/I6efHub1SN5KCJqLm1Ovx1/o.jpg" width={"222"} alt="카카오 로그인" /> </a>
                     </div>
                 </div>
             </div>  
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -35,16 +35,43 @@ function Home() {
         filter === "random" ? console.log("랜덤정렬") : console.log("좋아요순");
     }, [changeFilter]);
 
+    const checkTag = () => {
+        const tagBox = document.getElementsByName("tagBox");
+        tagBox.forEach(tag => { if(tag.checked) console.log(tag.value); });
+    }
+
     return(
         <>
             <Navigation/>
 
-            <select name="filter" onChange={changeFilter}>
-                <option value="mostlike"> 좋아요순 </option>
-                <option value="random"> 랜덤 보기 </option>
-            </select>
-
             <div className="homecontainer">
+                <div className="tagOptionBox">
+                    <p> 미슐간 주황색 글씨 들어갈 자리 </p>
+                    <p> 필터를 통해 원하는 이미지를 찾아보세요 </p>
+                    <form className="tagBox" onClick={checkTag}>
+                        <input name="tagBox" type="checkbox" value="어두운"/>어두운&nbsp;&nbsp;
+                        <input name="tagBox" type="checkbox" value="화사한"/>화사한&nbsp;&nbsp;
+                        <input name="tagBox" type="checkbox" value="다채로운"/>다채로운&nbsp;&nbsp;
+                        <input name="tagBox" type="checkbox" value="차분한"/>차분한&nbsp;&nbsp;
+                        <input name="tagBox" type="checkbox" value="강렬한"/>강렬한<br/>
+
+                        <input name="tagBox" type="checkbox" value="차가운"/>차가운&nbsp;&nbsp;
+                        <input name="tagBox" type="checkbox" value="따뜻한"/>따뜻한&nbsp;&nbsp;
+                        <input name="tagBox" type="checkbox" value="풍경"/>풍경&nbsp;&nbsp;
+                        <input name="tagBox" type="checkbox" value="동물"/>동물&nbsp;&nbsp;
+                        <input name="tagBox" type="checkbox" value="인물"/>인물&nbsp;&nbsp;
+                        <input name="tagBox" type="checkbox" value="기타"/>기타<br/>
+                    </form>
+                </div>
+
+                <div className="viewOptionBox">
+                    <button> 이미지 새로고침 </button>
+                    <select name="filter" onChange={changeFilter}>
+                        <option value="mostlike"> 좋아요순 </option>
+                        <option value="random"> 랜덤 보기 </option>
+                    </select>
+                </div>
+
                 <div className="drawingBox">
                     {
                         pictures.map((element) => 
