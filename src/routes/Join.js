0d5fbfb64b3d586543b88967c8f66c286f6d30d3
nijuy <youjin6325@naver.com>
2022-08-09 00:14:11 +0900

    별명 설정 페이지 디자인 적용

--- src/routes/Join.js
+++ src/routes/Join.js
@@ -7,14 +7,14 @@ function Join() {
         <>
         <Navigation />
 
-        <div>
-            <div> 미슐간 초록색 텍스트 로고 들어갈 자리 </div>
-            <div>‘Missul;GAN’에서 사용할 별명을 적어주세요.</div>
-            <div>
-                <input type="text" placeholder="한글, 영문으로 구성된 2자 이상 12자 이하의 별명을 적어주세요."/>
+        <div className="page-content">
+            <img className="logo" src="/img/textLogo2.png" width={200} alt=""/>
+            <div className="nicknameGuide">‘Missul;GAN’에서 사용할 별명을 적어주세요.</div>
+            <div className="nicknameBox">
+                <input type="text" placeholder="   한글, 영문으로 구성된 2자 이상 12자 이하의 별명을 적어주세요."/>
                 <button> 별명 설정 완료 </button>
-                <p> * 해당 별명은 이후 변경할 수 없습니다. </p>
             </div>
+            <p className="warning"> * 해당 별명은 이후 변경할 수 없습니다. </p>
         </div>
         </>
     );
