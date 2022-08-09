ee5b437a341062469315d32f2218af2ffa47bdd7
0chil <0@chll.it>
2022-08-09 13:29:47 +0900

    chore: oAuth 로그인 시 사용자 정보

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -4,12 +4,16 @@ import { useNavigate } from 'react-router-dom';
 function Login() {
     let aToken = new URL(window.location.href).searchParams.get("accessToken");
     let rToken = new URL(window.location.href).searchParams.get("refreshToken");
+    let profileNickname = new URL(window.location.href).searchParams.get("profileNickname");
+    let isFirstTime = new URL(window.location.href).searchParams.get("firstTime");
 
     let navigate = useNavigate();
 
     useEffect(() => {
         console.log("액세스토큰: " + aToken);
         console.log("리프레쉬토큰 : " + rToken);
+        console.log("프로파일 닉넴: " + profileNickname);
+        console.log("처음 접속?: " + isFirstTime);
         navigate('/join'); // 이미 가입했던 회원이면 받아서 /home으로 보내야할듯
       }, []);
 }
