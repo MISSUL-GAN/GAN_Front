e12ad657c3cdee4891fd122c48d97923579a085a
nijuy <youjin6325@naver.com>
2022-08-10 22:48:27 +0900

    사용자 정보 유지

--- src/index.js
+++ src/index.js
@@ -4,13 +4,25 @@ import './index.css';
 import App from './components/App';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist'; 
+import storageSession from 'redux-persist/lib/storage/session';
+import { applyMiddleware } from 'redux'; 
+import { PersistGate } from 'redux-persist/integration/react'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 const user = {nick:null, email:null, image:null, aToken:null, rToken:null};
+
+const persistConfig = {
+  key : root,
+  storage: storageSession,
+}
+
+const persistedReducer = persistReducer(persistConfig, myReducer);
 
 function myReducer(state = user, action){
     if(action.type === '로그인')
-      return {...user, nick:action.user.nick, email:action.user.email, image:action.user.image, aToken:action.user.aToken, rToken:action.user.rToken};
+      return {nick:action.user.nick, email:action.user.email, image:action.user.image, aToken:action.user.aToken, rToken:action.user.rToken};
     
     else if(action.type === '로그아웃')
       return {nick:null, email:null, image:null, aToken:null, rToken:null};
@@ -20,13 +32,16 @@ function myReducer(state = user, action){
       return {...user, nick:action.user.nick};
 
     else
-      return {...user};
+      return state;
   }
 
-const store = configureStore({ reducer: myReducer});
+const store = configureStore({reducer : persistedReducer}, applyMiddleware());
+const Persistor = persistStore(store);
 
 root.render(
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={Persistor}>
+        <App />
+      </PersistGate>
     </Provider>
 );
