d9ab2232e74e7212067e520ab400ab4c751c16e1
nijuy <youjin6325@naver.com>
2022-08-14 17:47:40 +0900

    feat: UserDrawing 컴포넌트 추가

---
+++ src/routes/UserDrawing.js
@@ -0,0 +1,157 @@
+import React, { useState } from "react";
+import { AUTH_URL } from '../LoginKey';
+import './UserDrawing.css';
+
+function UserDrawing({ ind, drawing }) {
+
+    const [like, setLike] = useState(false); // 서버에서 받은 정보로 초기값 넣기
+    const [bookmark, setBookmark] = useState(false); // 서버에서 받은 정보로 초기값 넣기
+    const [seeNFT, setSeeNFT] = useState(true);
+
+    function clickImg() {
+        const modal = document.getElementsByClassName("drawing-modal")[ind];
+        modal.style.display = "flex";
+        modal.style.top = `${window.scrollY}px`;
+        document.body.style.overflow = "hidden";
+    }
+
+    function clickClose() {
+        const modal = document.getElementsByClassName("drawing-modal")[ind];
+        modal.style.display = "none";
+        document.body.style.overflow = "unset";
+        if (!seeNFT) clickNFT();
+    }
+
+    function clickAlertClose() {
+        const modal = document.getElementById("alert-modal");
+        modal.style.display = "none";
+        document.body.style.overflow = "unset";
+    }
+
+    function clickLike() {
+        if (true)
+            setLike(!like);
+
+        else {
+            const modal = document.getElementById("alert-modal");
+            modal.style.top = `${window.scrollY}px`;
+            modal.style.display = "flex";
+        }
+    }
+
+    function clickBookmark() {
+        if (true)
+            setBookmark(!bookmark);
+
+        else {
+            const modal = document.getElementById("alert-modal");
+            modal.style.top = `${window.scrollY}px`;
+            modal.style.display = "flex";
+        }
+    }
+
+    function clickNFT() {
+        setSeeNFT(!seeNFT);
+        const NFTInfo = document.getElementsByClassName("NFTBox")[ind];
+        let see = seeNFT ? "inline" : "none";
+        NFTInfo.style.display = see;
+    }
+
+    return (
+        <>
+            <div className="imgwrapper">
+                <img src="/img/logo.png" width={100} height="auto" />
+
+                <div className="shadow" onClick={clickImg} />
+                <div className="titleBox">
+                    <p className="titleText"> 제목 {ind} </p>
+                    <div>
+                        <button className="like" onClick={clickLike}> <img src={like ? "/img/Like.png" : "/img/emptyLike.png"} width={32} alt="" /> </button>
+                        <button className="bookmark" onClick={clickBookmark}> <img src={bookmark ? "/img/bookmark.png" : "/img/emptyBookmark.png"} width={28} alt="" /> </button>
+                    </div>
+                </div>
+            </div>
+
+
+            <div id="zoom-modal" className="drawing-modal">
+                <div className="drawing-modal-window">
+                    <div className="drawing-modal-left"> <img className="large-drawing" src="/img/logo.png" alt="" /> </div>
+
+                    <div>
+                        <p className="drawing-modal-close" onClick={clickClose}> x </p>
+                        <div className="drawing-modal-right">
+                            <div>
+                                <div className="userInfo">
+                                    <img src="/img/logo.png" alt="" className="profileImg" width={50} height={50} />
+                                    <p className="author">
+                                        작가
+                                    </p>
+                                </div>
+
+                                <p className="drawing-title"> 제목 {ind} </p>
+
+                                <p className="description"> 설명 이 작품은 사람의 간을 본따서 만든 로고인데요 아주 귀엽게 생겼습니다 으아아아아아아아아아아아아ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ </p>
+
+                                <div className="drawing-tag">
+                                    <p> #태그1 </p> <p> #태그2 </p> <p> #태그3 </p>
+                                </div>
+                            </div>
+
+                            <div className="buttonBox">
+                                <button style={{ border: "none", backgroundColor: "rgb(0,0,0,0)" }}> <a href="/img/logo.png" download> <img src="/img/downloadIcon.png" width="60px" alt="" /> </a> </button>
+                                <button style={{ border: "none", backgroundColor: "rgb(0,0,0,0)" }}> <img src="/img/kakaoIcon.png" width="60px" alt="" /> </button>
+
+                                <button style={{
+                                    backgroundColor: "#3C6B50",
+                                    border: "none", borderRadius: "8px",
+                                    fontWeight: "200", color: "white",
+                                    width: "270px", height: "60px",
+                                    marginTop: "8px", marginRight: "30px", marginLeft: "5px"
+                                }} onClick={clickNFT}> OpenSea 통계 정보 </button>
+
+                                <div className="likeBox">
+                                    <button className="like" onClick={clickLike}> <img src={like ? "/img/Like.png" : "/img/emptyLike.png"} width={32} alt="" /> </button>
+                                    <p> 1234 </p>
+                                </div>
+
+                                <div className="bookmarkBox">
+                                    <button className="bookmark" onClick={clickBookmark}> <img src={bookmark ? "/img/bookmark.png" : "/img/emptyBookmark.png"} width={28} alt="" /> </button>
+                                    <p> 1234 </p>
+                                </div>
+                            </div>
+
+                            <div className="NFTBox">
+                                어쩌고저쩌고<br />
+                                이 작품의 NFT 가격!! 247239857198321093원<br />
+                                아무튼 통계 정보~~~ 들어갈 자리~~~
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div id="alert-modal" className="warning-modal">
+                <div className="warning-modal-window">
+                    <p className="warning-modal-close" onClick={clickAlertClose}> x </p>
+                    <div className="alert-content">
+                        <p> 로그인이 필요한 서비스입니다. </p>
+
+                        <div>
+                            <button id="kakaoLogin" onClick={() => { window.location.href = { AUTH_URL }; }}> <img src="/img/kakao.png" alt="" /> </button>
+                            <button id="googleLogin" onClick={() => { window.location.href = { AUTH_URL }; }}> <img src="/img/google.png" alt="" /> </button>
+                            <button id="naverLogin" onClick={() => { window.location.href = { AUTH_URL }; }}> <img src="/img/naver.png" alt="" /> </button>
+                        </div>
+
+                        <div>
+                            <p> 카카오 로그인 </p>
+                            <p> 구글 로그인 </p>
+                            <p> 네이버 로그인 </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default UserDrawing;
--- src/routes/userPage.js
+++ src/routes/userPage.js
@@ -1,252 +1,275 @@
-import React, { useEffect }  from "react";
+import React, { useEffect } from "react";
 import Navigation from "../components/Navigation";
-import Drawing from '../routes/Drawing';
+import UserDrawing from "./UserDrawing";
 import './UserPage.css';
 
 function UserPage() {
-    const member = {
-        nick: new URL(window.location.href).searchParams.get("member"),
-        img: new URL(window.location.href).searchParams.get("img"),
-        id: new URL(window.location.href).searchParams.get("id")
-    };
+  const member = {
+    nick: new URL(window.location.href).searchParams.get("member"),
+    img: new URL(window.location.href).searchParams.get("img"),
+    id: new URL(window.location.href).searchParams.get("id")
+  };
 
-    let testpic = [
+  let testpic = [
+    {
+      "createdAt": "2022-08-11 20:32:00",
+      "description": "망고.. 맛있음",
+      "fileName": "filename1",
+      "id": 1,
+      "member": {
+        "accountEmail": "vipyoujin@daum.net",
+        "id": 1,
+        "profileImage": "http://k.kakaocdn.net/dn/ctWVgw/btrHSMftNhz/jlmr9WNt7eGbHqW9wcsPH0/img_640x640.jpg",
+        "profileNickname": "유진",
+        "userNickname": "작가1"
+      },
+      "tags": [
         {
-          "createdAt": "2022-08-11 20:32:00",
-          "description": "망고.. 맛있음",
-          "fileName": "filename1",
           "id": 1,
-          "member": {
-            "accountEmail": "vipyoujin@daum.net",
-            "id": 1,
-            "profileImage": "http://k.kakaocdn.net/dn/ctWVgw/btrHSMftNhz/jlmr9WNt7eGbHqW9wcsPH0/img_640x640.jpg",
-            "profileNickname": "유진",
-            "userNickname": "작가1"
-          },
-          "tags": [
-            {
-              "id": 1,
-              "name": "어두운"
-            },
-            {
-                "id": 2,
-                "name": "화사한"
-            }
-          ],
-          "title": "첫번째 그림"
-    },
-    {
-        "createdAt": "2022-08-11 20:32:00",
-        "description": "망고.. 맛있음",
-        "fileName": "filename1",
-        "id": 1,
-        "member": {
-          "accountEmail": "vipyoujin@daum.net",
+          "name": "어두운"
+        },
+        {
           "id": 2,
-          "profileImage": "http://k.kakaocdn.net/dn/ctWVgw/btrHSMftNhz/jlmr9WNt7eGbHqW9wcsPH0/img_640x640.jpg",
-          "profileNickname": "유진",
-          "userNickname": "작가2"
-        },
-        "tags": [
-          {
-            "id": 1,
-            "name": "어두운"
-          },
-          {
-              "id": 2,
-              "name": "화사한"
-          }
-        ],
-        "title": "두번째 그림"
+          "name": "화사한"
+        }
+      ],
+      "title": "첫번째 그림"
     },
     {
-        "createdAt": "2022-08-11 20:32:00",
-        "description": "망고.. 맛있음",
-        "fileName": "filename1",
-        "id": 1,
-        "member": {
-          "accountEmail": "vipyoujin@daum.net",
-          "id": 3,
-          "profileImage": "http://k.kakaocdn.net/dn/ctWVgw/btrHSMftNhz/jlmr9WNt7eGbHqW9wcsPH0/img_640x640.jpg",
-          "profileNickname": "유진",
-          "userNickname": "작가3"
-        },
-        "tags": [
-          {
-            "id": 1,
-            "name": "어두운"
-          },
-          {
-              "id": 2,
-              "name": "화사한"
-          }
-        ],
-        "title": "세번째 그림"
-    },
-    {
-        "createdAt": "2022-08-11 20:32:00",
-        "description": "망고.. 맛있음",
-        "fileName": "filename1",
-        "id": 1,
-        "member": {
-          "accountEmail": "vipyoujin@daum.net",
-          "id": 4,
-          "profileImage": "http://k.kakaocdn.net/dn/ctWVgw/btrHSMftNhz/jlmr9WNt7eGbHqW9wcsPH0/img_640x640.jpg",
-          "profileNickname": "유진",
-          "userNickname": "작가4"
-        },
-        "tags": [
-          {
-            "id": 1,
-            "name": "어두운"
-          },
-          {
-              "id": 2,
-              "name": "화사한"
-          }
-        ],
-        "title": "네번째 그림"
-    },
-    {
-        "createdAt": "2022-08-11 20:32:00",
-        "description": "망고.. 맛있음",
-        "fileName": "filename1",
-        "id": 5,
-        "member": {
-          "accountEmail": "vipyoujin@daum.net",
+      "createdAt": "2022-08-11 20:32:00",
+      "description": "망고.. 맛있음",
+      "fileName": "filename1",
+      "id": 1,
+      "member": {
+        "accountEmail": "vipyoujin@daum.net",
+        "id": 2,
+        "profileImage": "http://k.kakaocdn.net/dn/ctWVgw/btrHSMftNhz/jlmr9WNt7eGbHqW9wcsPH0/img_640x640.jpg",
+        "profileNickname": "유진",
+        "userNickname": "작가2"
+      },
+      "tags": [
+        {
           "id": 1,
-          "profileImage": "http://k.kakaocdn.net/dn/ctWVgw/btrHSMftNhz/jlmr9WNt7eGbHqW9wcsPH0/img_640x640.jpg",
-          "profileNickname": "유진",
-          "userNickname": "작가5"
+          "name": "어두운"
         },
-        "tags": [
-          {
-            "id": 1,
-            "name": "어두운"
-          },
-          {
-              "id": 2,
-              "name": "화사한"
-          }
-        ],
-        "title": "다섯번째 그림"
+        {
+          "id": 2,
+          "name": "화사한"
+        }
+      ],
+      "title": "두번째 그림"
     },
     {
-        "createdAt": "2022-08-11 20:32:00",
-        "description": "망고.. 맛있음",
-        "fileName": "filename1",
-        "id": 1,
-        "member": {
-          "accountEmail": "vipyoujin@daum.net",
-          "id": 6,
-          "profileImage": "http://k.kakaocdn.net/dn/ctWVgw/btrHSMftNhz/jlmr9WNt7eGbHqW9wcsPH0/img_640x640.jpg",
-          "profileNickname": "유진",
-          "userNickname": "작가6"
+      "createdAt": "2022-08-11 20:32:00",
+      "description": "망고.. 맛있음",
+      "fileName": "filename1",
+      "id": 1,
+      "member": {
+        "accountEmail": "vipyoujin@daum.net",
+        "id": 3,
+        "profileImage": "http://k.kakaocdn.net/dn/ctWVgw/btrHSMftNhz/jlmr9WNt7eGbHqW9wcsPH0/img_640x640.jpg",
+        "profileNickname": "유진",
+        "userNickname": "작가3"
+      },
+      "tags": [
+        {
+          "id": 1,
+          "name": "어두운"
         },
-        "tags": [
-          {
-            "id": 1,
-            "name": "어두운"
-          },
-          {
-              "id": 2,
-              "name": "화사한"
-          }
-        ],
-        "title": "여섯번째 그림"
+        {
+          "id": 2,
+          "name": "화사한"
+        }
+      ],
+      "title": "세번째 그림"
     },
     {
-        "createdAt": "2022-08-11 20:32:00",
-        "description": "망고.. 맛있음",
-        "fileName": "filename1",
-        "id": 1,
-        "member": {
-          "accountEmail": "vipyoujin@daum.net",
-          "id": 7,
-          "profileImage": "http://k.kakaocdn.net/dn/ctWVgw/btrHSMftNhz/jlmr9WNt7eGbHqW9wcsPH0/img_640x640.jpg",
-          "profileNickname": "유진",
-          "userNickname": "작가7"
+      "createdAt": "2022-08-11 20:32:00",
+      "description": "망고.. 맛있음",
+      "fileName": "filename1",
+      "id": 1,
+      "member": {
+        "accountEmail": "vipyoujin@daum.net",
+        "id": 4,
+        "profileImage": "http://k.kakaocdn.net/dn/ctWVgw/btrHSMftNhz/jlmr9WNt7eGbHqW9wcsPH0/img_640x640.jpg",
+        "profileNickname": "유진",
+        "userNickname": "작가4"
+      },
+      "tags": [
+        {
+          "id": 1,
+          "name": "어두운"
         },
-        "tags": [
-          {
-            "id": 1,
-            "name": "어두운"
-          },
-          {
-              "id": 2,
-              "name": "화사한"
-          }
-        ],
-        "title": "일곱번째 그림"
+        {
+          "id": 2,
+          "name": "화사한"
+        }
+      ],
+      "title": "네번째 그림"
+    },
+    {
+      "createdAt": "2022-08-11 20:32:00",
+      "description": "망고.. 맛있음",
+      "fileName": "filename1",
+      "id": 5,
+      "member": {
+        "accountEmail": "vipyoujin@daum.net",
+        "id": 1,
+        "profileImage": "http://k.kakaocdn.net/dn/ctWVgw/btrHSMftNhz/jlmr9WNt7eGbHqW9wcsPH0/img_640x640.jpg",
+        "profileNickname": "유진",
+        "userNickname": "작가5"
+      },
+      "tags": [
+        {
+          "id": 1,
+          "name": "어두운"
+        },
+        {
+          "id": 2,
+          "name": "화사한"
+        }
+      ],
+      "title": "다섯번째 그림"
+    },
+    {
+      "createdAt": "2022-08-11 20:32:00",
+      "description": "망고.. 맛있음",
+      "fileName": "filename1",
+      "id": 1,
+      "member": {
+        "accountEmail": "vipyoujin@daum.net",
+        "id": 6,
+        "profileImage": "http://k.kakaocdn.net/dn/ctWVgw/btrHSMftNhz/jlmr9WNt7eGbHqW9wcsPH0/img_640x640.jpg",
+        "profileNickname": "유진",
+        "userNickname": "작가6"
+      },
+      "tags": [
+        {
+          "id": 1,
+          "name": "어두운"
+        },
+        {
+          "id": 2,
+          "name": "화사한"
+        }
+      ],
+      "title": "여섯번째 그림"
+    },
+    {
+      "createdAt": "2022-08-11 20:32:00",
+      "description": "망고.. 맛있음",
+      "fileName": "filename1",
+      "id": 1,
+      "member": {
+        "accountEmail": "vipyoujin@daum.net",
+        "id": 7,
+        "profileImage": "http://k.kakaocdn.net/dn/ctWVgw/btrHSMftNhz/jlmr9WNt7eGbHqW9wcsPH0/img_640x640.jpg",
+        "profileNickname": "유진",
+        "userNickname": "작가7"
+      },
+      "tags": [
+        {
+          "id": 1,
+          "name": "어두운"
+        },
+        {
+          "id": 2,
+          "name": "화사한"
+        }
+      ],
+      "title": "일곱번째 그림"
+    },
+    {
+      "createdAt": "2022-08-11 20:32:00",
+      "description": "망고.. 맛있음",
+      "fileName": "filename1",
+      "id": 1,
+      "member": {
+        "accountEmail": "vipyoujin@daum.net",
+        "id": 7,
+        "profileImage": "http://k.kakaocdn.net/dn/ctWVgw/btrHSMftNhz/jlmr9WNt7eGbHqW9wcsPH0/img_640x640.jpg",
+        "profileNickname": "유진",
+        "userNickname": "작가8"
+      },
+      "tags": [
+        {
+          "id": 1,
+          "name": "어두운"
+        },
+        {
+          "id": 2,
+          "name": "화사한"
+        }
+      ],
+      "title": "여덟번째 그림"
+    },
+    {
+      "createdAt": "2022-08-11 20:32:00",
+      "description": "망고.. 맛있음",
+      "fileName": "filename1",
+      "id": 1,
+      "member": {
+        "accountEmail": "vipyoujin@daum.net",
+        "id": 7,
+        "profileImage": "http://k.kakaocdn.net/dn/ctWVgw/btrHSMftNhz/jlmr9WNt7eGbHqW9wcsPH0/img_640x640.jpg",
+        "profileNickname": "유진",
+        "userNickname": "작가9"
+      },
+      "tags": [
+        {
+          "id": 1,
+          "name": "어두운"
+        },
+        {
+          "id": 2,
+          "name": "화사한"
+        }
+      ],
+      "title": "아홉번째 그림"
     }
-    ];
+  ];
 
-    useEffect(() => {
-        // /drawing/{member.id} 요청해서 그 사용자의 그림 다 받아오기
-    }, []);
+  useEffect(() => {
+    // /drawing/{member.id} 요청해서 그 사용자의 그림 다 받아오기
+  }, []);
 
-    return(
-        <>
-        <Navigation/>
-        
-        <div id="page-content">
-            <div> <img src={member.img} alt=""/> </div>
-            <div> {member.nick} </div>
-            <hr/>
+  return (
+    <>
+      <Navigation />
 
-            <div id="drawing-container">
-                <div id="drawingBox1">
-                    {
-                        testpic.map((element, index) =>
-                            <div className="imgwrapper">
-                                <img src="/img/logo.png"/>
-                                
-                                <div className="shadow"/>
-                                
-                                <div className="titleBox">
-                                    <p className="titleText"> {index} {element.title} </p>
-                                </div>
-                            </div>
-                        )
-                    }
-                </div>
+      <div id="page-content">
+        <div> <img src={member.img} alt="" /> </div>
+        <div> {member.nick} </div>
+        <hr />
 
-                <div id="drawingBox2">
-                    {
-                        testpic.map((element, index) =>
-                            <div className="imgwrapper">
-                                <img src="/img/logo.png"/>
-                                
-                                <div className="shadow"/>
-                                
-                                <div className="titleBox">
-                                    <p className="titleText"> {index} {element.title} </p>
-                                </div>
-                            </div>
-                        )
-                    }
-                </div>
+        <div id="drawing-container">
+          <div id="drawingBox1">
+            {
+              testpic.slice(0, 4).map((element, index) =>
+                <UserDrawing ind={index} />
+              )
+            }
+          </div>
 
-                <div id="drawingBox3">
-                    {
-                        testpic.map((element, index) =>
-                            <div className="imgwrapper">
-                                <img src="/img/logo.png"/>
-                                
-                                <div className="shadow"/>
-                                
-                                <div className="titleBox">
-                                    <p className="titleText"> {index} {element.title} </p>
-                                </div>
-                            </div>
-                        )
-                    }
-                </div>
-            </div>
+          <div id="drawingBox2">
+            {
+              testpic.slice(4, 6).map((element, index) =>
+                <UserDrawing ind={index + 4} />
+              )
+            }
+          </div>
 
+          <div id="drawingBox3">
+            {
+              testpic.slice(6,).map((element, index) =>
+                <UserDrawing ind={index + 6} />
+              )
+            }
+          </div>
         </div>
-        </>
-    );
+      </div>
+    </>
+  );
 }
 
 export default UserPage;
