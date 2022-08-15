92f395f7589d582ad6b0522049fbe18d63dfedd5
nijuy <youjin6325@naver.com>
2022-08-15 16:50:28 +0900

    fix: proxy 추가

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,39 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(
-        '/member/me',
+        '/member/*',
+        createProxyMiddleware({
+            target: 'https://api.missulgan.art:443',
+            changeOrigin: true,
+        })
+    );
+
+    app.use(
+        '/image/*',
+        createProxyMiddleware({
+            target: 'https://api.missulgan.art:443',
+            changeOrigin: true,
+        })
+    );
+
+    app.use(
+        '/heart/*',
+        createProxyMiddleware({
+            target: 'https://api.missulgan.art:443',
+            changeOrigin: true,
+        })
+    );
+
+    app.use(
+        '/scrap/*',
+        createProxyMiddleware({
+            target: 'https://api.missulgan.art:443',
+            changeOrigin: true,
+        })
+    );
+
+    app.use(
+        '/drawing/*',
         createProxyMiddleware({
             target: 'https://api.missulgan.art:443',
             changeOrigin: true,
