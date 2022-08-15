d926d2b988c8cdd7060a7134101420f3dc233b16
nijuy <youjin6325@naver.com>
2022-08-16 00:43:40 +0900

    시작 화면 변경

--- src/routes/Start.js
+++ src/routes/Start.js
@@ -3,20 +3,20 @@ import './Start.css';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 
-function Start(){
-    const user = useSelector( (state) => state );
+function Start() {
+    const user = useSelector((state) => state);
     const dispatch = useDispatch();
 
-    return(
+    return (
         <>
-        <div className='background'>
-            <div className="goHome">
-                <Link to = '/home'>
-                    <button type="button" className="btn btn-dark"> 감상하기 </button>
+            <div className='background'>
+                <Link to='/home'>
+                    <button type="button" className="startButton">
+                        <img src="/img/startButton.png" />
+                    </button>
                 </Link>
             </div>
-        </div>
-        </>  
+        </>
     );
 }
 
