036fff13d8bfec7302328826e6ad8c43c7ff07b6
0chil <0@chll.it>
2022-08-17 20:34:23 +0900

    chore: remove scripts

--- src/routes/ApiTestPage.js
+++ src/routes/ApiTestPage.js
@@ -11,11 +11,7 @@ function ApiTestPage() {
     const [tagIds, setTags] = useState([1, 2, 3]);
     return (
         <div>
-            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossOrigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossOrigin="anonymous"></script>
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossOrigin="anonymous"></link>
             <Navigation />
-
             <div className="container">
                 <div className="card" style={{ minHeight: "400px" }}>
                     <div className="card-body">
