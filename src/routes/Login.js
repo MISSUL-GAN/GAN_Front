00dc876bdf07edc83150f81fce9b97411bfdc766
0chil <0@chll.it>
2022-08-18 13:07:40 +0900

    fix: 로그인 오류

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,9 +1,10 @@
-import axios from "axios";
 import { useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { login } from "../redux/tokenReducer";
 import { setMember } from "../redux/memberReducer";
+import { getMember } from "../api/memberApi";
+import { saveToken } from '../util/tokenUtil';
+import { setAccessToken } from "../api/api";
 
 function Login() {
   const dispatch = useDispatch();
@@ -15,19 +16,13 @@ function Login() {
   const isFirstTime = new URL(window.location.href).searchParams.get("firstTime");
 
   useEffect(() => {
-    const loginDispatch = (token, member) => {
-      dispatch(login(token));
-      dispatch(setMember(member));
-    };
-    axios.get("https://api.missulgan.art/member/me", {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    })
-      .then(res => {
-        const { data: member } = res;
-        loginDispatch({ accessToken, refreshToken }, member);
-        if (isFirstTime === true) //isFirstTime 들어갈 자리
+    const loginDispatch = (member) => dispatch(setMember(member));
+    setAccessToken(accessToken);
+    saveToken(accessToken, refreshToken);
+    getMember()
+      .then(data => {
+        loginDispatch(data);
+        if (isFirstTime === true)
           navigate(`/join/${name}`);
         else
           navigate('/home');
