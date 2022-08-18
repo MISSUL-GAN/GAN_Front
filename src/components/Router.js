45924734dd535c8ef622bd54d4879d035e9bbff1
0chil <0@chll.it>
2022-08-19 00:55:32 +0900

    feat: 인증된 사용자 Route

--- src/components/Router.js
+++ src/components/Router.js
@@ -9,6 +9,7 @@ import Join from '../routes/Join';
 import CreateDrawing from '../routes/CreateDrawing';
 import ApiTestPage from "../routes/ApiTestPage";
 import WithNav from "./WithNav";
+import AuthRoute from "./AuthRoute";
 
 function GalleryRouter() {
     return (
@@ -17,12 +18,14 @@ function GalleryRouter() {
                 <Route exact path="/login/oauth" element={<Login />} />
                 <Route exact path='/' element={<Start />} />
                 <Route element={<WithNav />}>
-                    <Route exact path="/join/:initialName" element={<Join />} />
+                    <Route element={<AuthRoute />}>
+                        <Route exact path='/create' element={<CreateDrawing />} />
+                        <Route exact path="/join/:initialName" element={<Join />} />
+                        <Route exact path='/myPage' element={<MyPage />} />
+                        <Route exact path='/apitest' element={<ApiTestPage />} />
+                    </Route>
                     <Route exact path='/home' element={<Home />} />
-                    <Route exact path='/myPage' element={<MyPage />} />
                     <Route exact path='/userPage' element={<UserPage />} />
-                    <Route exact path='/create' element={<CreateDrawing />} />
-                    <Route exact path='/apitest' element={<ApiTestPage />} />
                 </Route>
             </Routes>
         </Router>
