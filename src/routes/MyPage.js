8fefec4fe7e3af07c8cd8fbcb97bf2256b9a27cc
nijuy <youjin6325@naver.com>
2022-08-19 22:45:20 +0900

    fix: 오타 수정

--- src/routes/MyPage.js
+++ src/routes/MyPage.js
@@ -3,7 +3,6 @@ import { getDrawings, deleteDrawing } from "../api/drawingApi";
 import { getScrap, unscrap } from "../api/scrapApi";
 import Navigation from "../components/Navigation";
 import UserDrawing from '../routes/UserDrawing';
-import isLogin from "../isLogin";
 import './MyPage.css';
 
 function MyPage() {
@@ -12,7 +11,7 @@ function MyPage() {
     const [myscrap, setMyScrap] = useState([]);
 
     useEffect(() => {
-        async function getMyPageDrawings(){
+        async function getMyPageDrawings() {
             setMyDrawing(await getDrawings());
             setMyScrap(await getScrap());
         }
@@ -34,7 +33,7 @@ function MyPage() {
     }
 
     const clickDelete = (id) => {
-        if(option === "my"){
+        if (option === "my") {
             setMyDrawing(mydrawing.filter((item) => item.id !== id));
             deleteDrawing(id);
         }
@@ -50,72 +49,68 @@ function MyPage() {
     }
 
     return (
-        <>
-        <Navigation />
+        <div id="my-page-content">
+            <div id="tabBox">
+                <button id="my" onClick={clickOption}>
+                    내 작품
+                    <hr />
+                </button>
 
-                    <div id="my-page-content">
-                        <div id="tabBox">
-                            <button id="my" onClick={clickOption}>
-                                내 작품
-                                <hr />
-                            </button>
+                <button id="bookmark" onClick={clickOption}>
+                    스크랩한 작품
+                    <hr />
+                </button>
+            </div>
 
-                            <button id="bookmark" onClick={clickOption}>
-                                스크랩한 작품
-                                <hr />
-                            </button>
+            <div id="drawing-container">
+                {mydrawing.length === 0
+                    ?
+                    <p id="nodrawing"> 아직 작품이 존재하지 않습니다. </p>
+                    :
+                    <>
+                        <div id="drawingBox1">
+                            {option === "my"
+                                ?
+                                mydrawing.slice(0, 4).map((element, index) =>
+                                    <UserDrawing key={element.id} ind={index} drawing={element} mine={true} clickDelete={clickDelete} />
+                                )
+                                :
+                                myscrap.slice(0, 4).map((element, index) =>
+                                    <UserDrawing key={element.id} ind={index} drawing={element} clickDelete={clickDelete} />
+                                )
+                            }
                         </div>
 
-                        <div id="drawing-container">
-                            {mydrawing.length === 0
+                        <div id="drawingBox2">
+                            {option === "my"
                                 ?
-                                <p id="nodrawing"> 아직 작품이 존재하지 않습니다. </p>
+                                mydrawing.slice(4, 6).map((element, index) =>
+                                    <UserDrawing key={element.id} ind={index + 4} drawing={element} mine={true} clickDelete={clickDelete} />
+                                )
                                 :
-                                <>
-                                    <div id="drawingBox1">
-                                        {option === "my"
-                                            ?
-                                            mydrawing.slice(0, 4).map((element, index) =>
-                                                <UserDrawing key={element.id} ind={index} drawing={element} mine={true} clickDelete={clickDelete} />
-                                            )
-                                            :
-                                            myscrap.slice(0, 4).map((element, index) =>
-                                                <UserDrawing key={element.id} ind={index} drawing={element} clickDelete={clickDelete}/>
-                                            )
-                                        }
-                                    </div>
+                                myscrap.slice(4, 6).map((element, index) =>
+                                    <UserDrawing key={element.id} ind={index + 4} drawing={element} clickDelete={clickDelete} />
+                                )
+                            }
+                        </div>
 
-                                    <div id="drawingBox2">
-                                        {option === "my"
-                                            ?
-                                            mydrawing.slice(4, 6).map((element, index) =>
-                                                <UserDrawing key={element.id} ind={index + 4} drawing={element} mine={true} clickDelete={clickDelete} />
-                                            )
-                                            :
-                                            myscrap.slice(4, 6).map((element, index) =>
-                                                <UserDrawing key={element.id} ind={index + 4} drawing={element} clickDelete={clickDelete}/>
-                                            )
-                                        }
-                                    </div>
-
-                <div id="drawingBox3">
-                    {option === "my"
-                        ?
-                        testpic.slice(6,).map((element, index) =>
-                            <UserDrawing key={element.id} ind={index + 6} drawing={element} mine={true} clickDelete={clickDelete}/>
-                        )
-                        :
-                        testpic2.slice(6,).map((element, index) =>
-                            <UserDrawing key={element.id} ind={index + 6} drawing={element}/>
-                        )
-                    }
-                </div>
+                        <div id="drawingBox3">
+                            {option === "my"
+                                ?
+                                mydrawing.slice(6,).map((element, index) =>
+                                    <UserDrawing key={element.id} ind={index + 6} drawing={element} mine={true} clickDelete={clickDelete} />
+                                )
+                                :
+                                myscrap.slice(6,).map((element, index) =>
+                                    <UserDrawing key={element.id} ind={index + 6} drawing={element} />
+                                )
+                            }
+                        </div>
+                    </>
+                }
             </div>
         </div>
-
-        </>
     );
-
 }
 
 export default MyPage;
