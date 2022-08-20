14a6306829021401e7af72c7cc40ca37a4dfb9ac
0chil <0@chll.it>
2022-08-20 22:26:11 +0900

    feat: 그림 보여주는 Route

--- src/components/DrawingRoute.js
+++ src/components/DrawingRoute.js
@@ -1,22 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { useParams, useNavigate } from 'react-router';
 import { getDrawing } from '../api/drawingApi';
+import LoginAlert from './LoginAlert';
+import DetailModal from "../routes/DetailModal";
 
 const DrawingRoute = () => {
     const navigate = useNavigate();
+    const navigateToHome = useCallback(() => navigate('/home'), [navigate]);
+
     const { drawingId } = useParams();
     const [drawing, setDrawing] = useState(null);
 
+    const [loginAlertExpanded, setLoginAlertExpanded] = useState(false);
+    const handleLoginAlertClose = () => { setLoginAlertExpanded(false); }
+    const handleLoginAlertOpen = () => { setLoginAlertExpanded(true); }
+
+    const handleDetailModalClose = navigateToHome;
+
+
     useEffect(() => {
-        const navigateToHome = () => navigate('..');
         getDrawing(drawingId)
-            .then(res => setDrawing(res))
+            .then(setDrawing)
             .catch(navigateToHome)
-    }, [drawingId, navigate]);
+    }, [drawingId, navigateToHome]);
 
     return (
         <>
-            <>{JSON.stringify(drawing)}</>
+            {drawing && (
+                <DetailModal drawing={drawing} handleDetailModalClose={handleDetailModalClose} openLoginAlert={handleLoginAlertOpen} />
+            )}
+            {loginAlertExpanded && (
+                <LoginAlert handleLoginAlertClose={handleLoginAlertClose} />
+            )}
         </>
     );
 };
