5e30fe80c236baad6a092294e9f2a41480cac5c1
nijuy <youjin6325@naver.com>
2022-08-21 02:48:50 +0900

    배경화면 조절

--- src/routes/Start.js
+++ src/routes/Start.js
@@ -8,13 +8,22 @@ function Start() {
     const dispatch = useDispatch();
 
     return (
-        <div className="background-repeat">
-            <div className='background'>
-                <Link to='/home'>
-                    {/* <button type="button" className="startButton">
-                        <img src="/img/startButton.png" />
-                    </button> */}
-                </Link>
+        <div className="background">
+            <div id="content-box">
+                <div id="leftBox">
+                    <img src="img/start-logo.png" id="logo" />
+                    <img src="img/start-description.png" id="description" />
+
+                    <Link to='/home'>
+                        <button type="button">
+                            <img src="/img/startButton.png" id="start-button" />
+                        </button>
+                    </Link>
+                </div>
+
+                <div id="rightBox">
+                    <img src="img/start-image.png" id="image" />
+                </div>
             </div>
         </div>
     );
@@ -22,3 +31,17 @@ function Start() {
 
 export default Start;
 
+/*                    <img src="img/start-logo.png" id="logo"/>
+                    <img src="img/start-description.png" id="description"/>
+
+                <img src="img/start-image.png" id="image"/>
+
+            <div className='background'/>
+
+                <Link to='/home'>
+                    <button type="button" className="startButton">
+                        <img src="/img/startButton.png" />
+                    </button> 
+                </Link>
+
+*/
