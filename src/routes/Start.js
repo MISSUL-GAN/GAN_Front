8edea503e8c0f277addf2e7e1a18730c51ba9050
이유진 <youjin6325@naver.com>
2022-08-23 22:41:33 +0900

    Revert "chore: img 태그 alt 추가, 필요없는 변수 삭제"

--- src/routes/Start.js
+++ src/routes/Start.js
@@ -1,24 +1,28 @@
 import React from "react";
 import './Start.css';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 
 function Start() {
+    const user = useSelector((state) => state);
+    const dispatch = useDispatch();
+
     return (
         <div className="background">
             <div id="content-box">
                 <div id="leftBox">
-                    <img src="img/start-logo.png" id="logo" alt=""/>
-                    <img src="img/start-description.png" id="description" alt=""/>
+                    <img src="img/start-logo.png" id="logo" />
+                    <img src="img/start-description.png" id="description" />
 
                     <Link to='/home'>
                         <button type="button">
-                            <img src="/img/startButton.png" id="start-button" alt=""/>
+                            <img src="/img/startButton.png" id="start-button" />
                         </button>
                     </Link>
                 </div>
 
                 <div id="rightBox">
-                    <img src="img/start-image.png" id="image" alt=""/>
+                    <img src="img/start-image.png" id="image" />
                 </div>
             </div>
         </div>
