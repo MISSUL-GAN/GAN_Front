d24024bb6276d324b0431d0e26ee7fccd49e5f72
0chil <0@chll.it>
2022-08-30 04:59:11 +0900

    feat: 도움말

--- src/components/WalletHelpModal.js
+++ src/components/WalletHelpModal.js
@@ -2,6 +2,7 @@ import React from "react";
 import ModalElement from "./ModalElement";
 import detectEthereumProvider from '@metamask/detect-provider';
 import style from 'styled-components';
+import { useNavigate, useOutletContext } from "react-router-dom";
 
 const ShadowImage = style.img`
     box-shadow: 4px 4px 10px 4px rgb(0 0 0 / 15%);
@@ -31,7 +32,7 @@ const Step = style.div`
     margin: 20px 0;
 `;
 
-const H2WithLogo = style.h2`
+const H2WithLogo = style.h3`
     display: flex;
     align-items: center;
     gap: 10px;
@@ -59,7 +60,11 @@ const Button = style.button`
     }
 `;
 
-const WalletHelpModal = ({ open, handleClose, openAlert }) => {
+const WalletHelpModal = () => {
+    const { openAlert } = useOutletContext();
+
+    const navigate = useNavigate();
+    const handleClose = () => navigate('..');
 
     const addNetwork = async () => {
         try {
@@ -91,7 +96,7 @@ const WalletHelpModal = ({ open, handleClose, openAlert }) => {
     };
 
     return (
-        <ModalElement open={open} handleClose={handleClose}>
+        <ModalElement handleClose={handleClose}>
             <h1>지갑 만들기</h1>
             <Step>
                 <H2WithLogo>
@@ -99,12 +104,9 @@ const WalletHelpModal = ({ open, handleClose, openAlert }) => {
                     MetaMask 설치
                 </H2WithLogo>
                 <ShadowImage src="/img/Metamask-install.gif" alt="" width="100%" />
-                {/* <Instruction><Number>1</Number>우측 상단에 있는 지갑 아이콘을 클릭해주세요.</Instruction>
-                <Instruction><Number>2</Number>‘MetaMask’ 탭을 클릭해주세요.</Instruction>
-                <Instruction><Number>3</Number>사용자 환경에 맞는 메타마스크 프로그램을 설치해주세요.</Instruction> */}
                 <Instruction>
                     <Number>1</Number>
-                    <a href="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn" target="_blank" rel="noreferrer">MetaMask 크롬 확장 프로그램</a>을 설치해주세요.
+                    <a href="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn" target="_blank" rel="noreferrer">MetaMask 크롬 확장 프로그램</a>을 설치해주세요
                 </Instruction>
             </Step>
             <Step>
@@ -113,13 +115,31 @@ const WalletHelpModal = ({ open, handleClose, openAlert }) => {
                     Ethereum 지갑 생성
                 </H2WithLogo>
                 <ShadowImage src="/img/Metamask-make-wallet.gif" alt="" width="100%" />
-                <Instruction><Number>1</Number><b>MetaMask</b>를 실행해주세요.</Instruction>
-                <Instruction><Number>2</Number><b>시작하기</b> 버튼을 클릭해주세요.</Instruction>
-                <Instruction><Number>3</Number><b>동의함</b> 버튼을 클릭해주세요.</Instruction>
-                <Instruction><Number>4</Number><b>지갑 생성</b> 버튼을 클릭해주세요.</Instruction>
-                <Instruction><Number>5</Number>비밀번호를 설정하고, 약관에 동의한 후 <b>생성</b>버튼을 클릭해주세요.</Instruction>
-                <Instruction><Number>6</Number><Button onClick={addNetwork}>이곳</Button>을 눌러 폴리곤 네트워크를 추가해 주세요.</Instruction>
+                <Instruction><Number>1</Number><b>MetaMask</b>를 실행해주세요</Instruction>
+                <Instruction><Number>2</Number><b>시작하기</b> 버튼을 클릭해주세요</Instruction>
+                <Instruction><Number>3</Number><b>괜찮습니다</b> 버튼을 클릭해주세요</Instruction>
+                <Instruction><Number>4</Number><b>지갑 생성</b> 버튼을 클릭해주세요</Instruction>
+                <Instruction><Number>5</Number>비밀번호를 설정하고, 약관에 동의한 후 <b>생성</b>버튼을 클릭해주세요</Instruction>
+                <Instruction><Number>6</Number>비밀 복구 질문을 <b>저장</b>해주세요</Instruction>
+                <Instruction><Number>7</Number><b>새로고침</b> 해주세요. 변환된 작품은 <b>유지</b>됩니다</Instruction>
             </Step>
+            <Step>
+                <H2WithLogo>
+                    <img src="/img/polygon.svg" alt="" />
+                    폴리곤 네트워크 추가 (선택사항)
+                </H2WithLogo>
+                <ShadowImage src="/img/Polygon-network-install.gif" alt="" width="100%" />
+                <Instruction><Number>1</Number><Button onClick={addNetwork}>이곳</Button>을 눌러 폴리곤 네트워크를 추가해 주세요</Instruction>
+            </Step>
+            <hr />
+            <h1>지갑 활용하기</h1>
+            <H2WithLogo>
+                <img src="/img/metamask.png" alt="" />
+                내 지갑 입력
+            </H2WithLogo>
+            <ShadowImage src="/img/Wallet-input.gif" alt="" width="100%" />
+            <Instruction><Number>!</Number>작품 변환 시 지갑을 선택할 수 있어요</Instruction>
+            <hr />
         </ModalElement>
     );
 };
