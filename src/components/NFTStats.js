f3b441ee61dd2561f7a9799b84c7da2420debbfb
0chil <0@chll.it>
2022-08-31 19:39:38 +0900

    feat: NFT 정보

--- src/components/NFTStats.js
+++ src/components/NFTStats.js
@@ -1,69 +1,123 @@
-import React, { useState } from "react";
+import { Card, CircularProgress, ClickAwayListener, Paper, Skeleton, styled as style, Tooltip, tooltipClasses } from "@mui/material";
+import { width } from "@mui/system";
+import React, { useCallback, useEffect, useState } from "react";
 import styled from 'styled-components';
 import { getMintedNFT, getOpenseaPermalink, getTokenIdOwners, getWalletTokenIdTransfers } from "../api/nftApi";
 
-const HiddenInput = styled.input`
-  display: none;
-`;
+const NFTButton = styled.button`
+    display: flex;
+    flex-grow: 1;
+    height: 60px;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    font-weight: 300;
+    font-size: 18px;
+    background: #3C6B50;
+    border: none;
+    border-radius: 8px;
+    color: #FFFFFF;
+    max-width: 200px;
+    padding-right: 0px !important;
 
-const Tag = styled.div`
-    & {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        padding: 0px 20px;
-        height: 40px;
-        background: #F4F4F4;
-        border-radius: 40px;
-        color: #3C6B50;
-        cursor: pointer;
-
-        margin-right: 10px;
-        margin-top: 10px;
-    }
-
-    ${HiddenInput}:checked + & {
-        color: #FFFFFF;
-        background: #3C6B50;
+    &:disabled {
+        opacity: 0.8;
     }
 `;
 
+const LoadingIndicator = style(CircularProgress)`
+    color: white;
+    margin-right: 10px;
+    &:disabled {
+        display: none;
+    }
+`
+
+const NFTButtonDiv = styled.div`
+    display: flex;
+    flex-grow: 1;
+    max-width: 200px;
+`;
+
+const OpenseaIcon = styled.button`
+    min-width:60px;
+    width: 60px;
+    height: 60px;
+    background-image: url('/img/openseaIcon.png');
+    background-size: contain;
+
+    &:disabled {
+        opacity: 0.6;
+    }
+`
+
+const Address = styled.a`
+    display: block;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+`
+
+const PaddedPaper = styled(Card)`
+    padding: 10px;
+`;
+
+const NFTTooltip = style(({ className, ...props }) => (
+    <Tooltip {...props} arrow placement="top-start" classes={{ popper: className }} />
+))(({ theme }) => ({
+    [`& .${tooltipClasses.arrow}`]: {
+        color: "white",
+        filter: "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.15))"
+    },
+    [`& .${tooltipClasses.tooltip}`]: {
+        backgroundColor: 'white',
+        color: 'rgba(0, 0, 0, 0.87)',
+        padding: "12px 16px",
+        fontFamily: 'Spoqa Han Sans Neo',
+        fontWeight: 500,
+        fontSize: "14px",
+        borderRadius: "8px",
+        filter: "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.15))",
+        maxWidth: "300px",
+    },
+}));
+
+const NULL_ADDRESS = "0x0000000000000000000000000000000000000000";
 
 const NFTStats = ({ nftTransactionHash }) => {
 
-    const [seeNFT, setSeeNFT] = useState(true);
-    const [nftInfo, setNftInfo] = useState([]);
+    const [nftInfo, setNftInfo] = useState({ owners: [], transfers: [], permalink: null });
+    const [isLoading, setIsLoading] = useState(false);
+    const [tooltipOpen, setTooltipOpen] = useState(false);
 
-    async function getDrawingNFTInfo() {
+    const getDrawingNFTInfo = useCallback(async function () {
+        if ( !nftTransactionHash || nftInfo.owners.length) return false;
+        setIsLoading(true);
         const { contract_address: contractAddress, token_id: tokenId } = await getMintedNFT(nftTransactionHash);
         const owners = await getTokenIdOwners(contractAddress, tokenId);
         const transfers = await getWalletTokenIdTransfers(contractAddress, tokenId);
         const permalink = await getOpenseaPermalink(contractAddress, tokenId);
-        const nftInfo = { owners, transfers, permalink };
-        setNftInfo(nftInfo);
+        const nftStat = { owners, transfers, permalink };
+        setNftInfo(nftStat);
+        setIsLoading(false);
+    }, [nftInfo.owners.length, nftTransactionHash]);
+
+    async function clickNFT() {
+        if (!isLoading && nftInfo.owners.length)
+            setTooltipOpen(true);
     }
 
-    function clickNFT() {
-        setSeeNFT(state => !state);
-        if (seeNFT)
-            getDrawingNFTInfo();
-    }
-
-    return (
+    const NFTInfoFragment = (
         <>
-            <button id="open-nft-button" onClick={clickNFT}> NFT 통계 정보 </button>
             <div>
-                <a href={nftInfo.permalink} target="_blank" rel="noreferrer"> OpenSea에서 보기 </a>
-                <div>소유자 목록</div>
+                <strong>소유자 목록</strong>
                 <div>
                     {
                         nftInfo.owners.map(owner => (
-                            <div>
-                                {/* <img src="https://openseauserdata.com/files/6f8e2979d428180222796ff4a33ab929.svg" width={20} alt="" /> */}
-                                <a href={`https://opensea.io/${owner.owner_of}`} target="_blank" rel="noreferrer">{owner.owner_of}</a>
-                                <p>{owner.amount}개</p>
-                            </div>
+                            <PaddedPaper key="owner">
+                                <Address href={`https://opensea.io/${owner.owner_of}`} target="_blank" rel="noreferrer">{owner.owner_of}</Address>
+                                <strong>{owner.amount}개</strong>
+                            </PaddedPaper>
                         ))
                     }
                 </div>
@@ -71,20 +125,43 @@ const NFTStats = ({ nftTransactionHash }) => {
             <hr />
 
             <div>
-                최근 거래 내역 <br />
+                <strong>최근 거래 내역</strong>
                 {
                     nftInfo.transfers.map(transfer => (
-                        <div>
-                            {`${transfer.from_address} => ${transfer.to_address}`}<br />
-                            {new Date(transfer.block_timestamp).toLocaleString()}<br />
-                            {transfer.amount}개
-                        </div>
+                        <PaddedPaper key="transfer">
+                            <Address href={transfer.from_address === NULL_ADDRESS ? null : `https://opensea.io/${transfer.from_address}`} target="_blank">{transfer.from_address === NULL_ADDRESS ? "널 주소" : transfer.from_address}</Address>
+                            <span>⬇</span>
+                            <Address href={`https://opensea.io/${transfer.to_address}`} target="_blank">{transfer.to_address}</Address>
+                            <strong>{transfer.amount}개</strong> {new Date(transfer.block_timestamp).toLocaleString()}
+                        </PaddedPaper>
                     ))
                 }
-                {/* {nftInfo.last_sale.event_timestamp.replace("T", " ")} /
-                                                        사용자 {nftInfo.last_sale.transaction.from_account.user.username}가
-                                                        0.{nftInfo.last_sale.total_price.substring(0, 2)} {nftInfo.last_sale.payment_token.symbol}에 거래 */}
             </div>
+        </>
+    );
+
+    useEffect(() => {
+        getDrawingNFTInfo()
+    }, [getDrawingNFTInfo]);
+
+    return (
+        <>
+        <OpenseaIcon disabled={!nftTransactionHash || isLoading} onClick={()=>window.open(nftInfo.permalink)}/>
+            <ClickAwayListener onClickAway={() => setTooltipOpen(false)}>
+                <NFTButtonDiv>
+                    <NFTTooltip
+                        open={tooltipOpen}
+                        title={NFTInfoFragment}
+                        onClose={() => setTooltipOpen(false)}
+                        className="NFTButton"
+                        disableFocusListener
+                        disableHoverListener
+                        disableTouchListener
+                    >
+                        <NFTButton className="NFTButton" onClick={clickNFT} disabled={!nftTransactionHash || isLoading}><LoadingIndicator hidden={!isLoading} size="24px"/> NFT 정보 </NFTButton>
+                    </NFTTooltip>
+                </NFTButtonDiv>
+            </ClickAwayListener>
         </>
     );
 };
